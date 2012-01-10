06c09fe1e8fbaca7e784696c46617cddb2a3add7
Frank de Brabander <debrabander@gmail.com>
2012-01-10 21:02:00 +0100

    Change code format of the example files.

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -1,17 +1,20 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
-  lcd.init();                      // initialize the lcd 
- 
-  // Print a message to the LCD.
-  lcd.backlight();
-  lcd.print("Hello, world!");
+	// initialize the LCD
+	lcd.init();
+
+	// Turn on the blacklight and print a message.
+	lcd.backlight();
+	lcd.print("Hello, world!");
 }
 
 void loop()
 {
+	// Do nothing here...
 }
